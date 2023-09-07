5aa6fb43d32b779eaafebabd9505aab9f96ae884
Tamas Kemenczy <tkemenczy@gmail.com>
2023-09-07 12:22:44 -0500

    4 spaces for c#

--- codestyle.el
+++ codestyle.el
@@ -1,10 +1,12 @@
 ;; (add-hook 'csharp-mode-hook (lambda () (setq comment-start "/*" comment-end "*/")))
 
-(add-hook 'html-mode-hook (lambda () (set (make-local-variable 'sgml-basic-offset) 2)))
+(add-hook 'html-mode-hook (lambda () (set (make-local-variable 'sgml-basic-offset) 4)))
 
 (setq shader-indent-offset 4)
 
 (defun my-c-mode-hook ()
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0)
   (c-set-offset 'arglist-cont-nonempty '+)
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'arglist-close '0))
