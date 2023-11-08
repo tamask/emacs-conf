313dc18cfb6a22b2653c9ada06ba8c9472a886a4
Tamas Kemenczy <tkemenczy@gmail.com>
2023-11-08 14:09:14 -0600

    Switched to using straight.el
    Now packages like `gdscript-mode` and `gdshader-mode` can be pulled
    from their github repos automatically.

--- init.el
+++ init.el
@@ -2,17 +2,26 @@
 ;; init
 ;;
 
+;; straight.el bootstrap
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
 ;; bootstrap load-relative package
 
-(package-initialize)
-
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'load-relative)
-  (package-install 'load-relative))
-
-(require 'load-relative)
+(use-package load-relative :straight t)
 
 ;; platform specifics
 
