cfa5b211b9f972bd876cab6e517d0d005d8d1753
Tamas Kemenczy <tkemenczy@gmail.com>
2023-11-08 15:22:09 -0600

    Only apply linux/gnome window padding when in window-system

--- linux.el
+++ linux.el
@@ -2,9 +2,10 @@
 ;; linux
 ;;
 
-(setq-default left-margin-width 1 right-margin-width 1)
-(set-window-buffer nil (current-buffer))
-(set-window-margins (selected-window) 1 1)
+(when window-system
+  (setq-default left-margin-width 1 right-margin-width 1)
+  (set-window-buffer nil (current-buffer))
+  (set-window-margins (selected-window) 1 1))
 
 (defun linux-ui-dark-mode ()
   (string= (string-trim (shell-command-to-string "gsettings get org.gnome.desktop.interface color-scheme")) "'prefer-dark'"))
