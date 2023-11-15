9277927387a354f46a9965ed8efbecdc094510d2
Tamas Kemenczy <tkemenczy@gmail.com>
2023-11-14 23:58:50 -0600

    Made titlebar transparent and empty on macOS

--- macos.el
+++ macos.el
@@ -9,5 +9,13 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 
+;; minimalist titlebar
+
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+;; detect whether we're in dark mode
+
 (defun macos-ui-dark-mode ()
   (string= (string-trim (shell-command-to-string "defaults read -g AppleInterfaceStyle")) "Dark"))
